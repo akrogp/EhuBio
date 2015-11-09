feb7f657ea1f96f7200b75932183886ce82c5f86
Gorka Prieto <gorka.prieto@gmail.com>
2015-11-09 21:13:47 +0100

    Included number utilities

---
+++ Projects/java/EhuBio/src/es/ehubio/Numbers.java
@@ -0,0 +1,15 @@
+package es.ehubio;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class Numbers {
+	public static double parseDouble( String str ) throws ParseException {
+		return NumberFormat.getInstance(Locale.ENGLISH).parse(str).doubleValue();
+	}
+	
+	public static String toString( double num ) {
+		return String.format(Locale.ENGLISH, "%f", num);
+	}
+}
