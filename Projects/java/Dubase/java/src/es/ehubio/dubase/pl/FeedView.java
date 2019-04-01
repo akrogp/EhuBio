12084c4ee0a8269e2879cd1da720cb03b3ea6702
Gorka Prieto <gorka.prieto@ehu.eus>
2019-04-01 11:08:24 +0200

    Included USP11 data

--- Projects/java/Dubase/java/src/es/ehubio/dubase/pl/FeedView.java
+++ Projects/java/Dubase/java/src/es/ehubio/dubase/pl/FeedView.java
@@ -25,8 +25,8 @@ public class FeedView implements Serializable {
 	}
 	
 	public void saveExamples() {
-		//String[] genes = {"USP1", "USP7", "USP9X"};
-		String[] genes = {"USP7"};
+		//String[] genes = {"USP1", "USP7", "USP9X", "USP11"};
+		String[] genes = {"USP11"};
 		for( String gene : genes )
 			try {
 				saveTest(gene);
