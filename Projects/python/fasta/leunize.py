0de875b346e11adf833b26a5e153e0ef7da82d24
Gorka Prieto <gorka.prieto@ehu.eus>
2019-07-03 20:07:55 +0200

    Tool for replacing I/L in fasta files

---
+++ Projects/python/fasta/leunize.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+import re
+
+if len(sys.argv) != 2:
+	print("Usage: {} <database.fasta>".format(sys.argv[0]))
+	sys.exit()
+
+with open(sys.argv[1]) as fasta:
+	for line in fasta:
+		if line[0] != '>':
+			line = re.sub('[iI]','L',line)
+		print(line, end='')
